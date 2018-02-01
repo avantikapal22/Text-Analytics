e93758205bb319ff1de116b9006ab73c5dafbe35
avantikapal22 <avantikapal22@gmail.com>
2018-02-01 20:13:59 +0530

    Update SentimentAnalysis.R

--- SentimentAnalysis.R
+++ SentimentAnalysis.R
@@ -52,3 +52,25 @@ senti_nrc<-function(corpus){
 }
 
 # Function ends
+
+
+
+
+senti_nrc1<-function(corpus){
+  
+  textdf = data_frame(text = corpus) # converting text data into dataframe  
+  
+  senti<-  textdf %>%
+    mutate(Doc_No = 1:nrow(textdf)) %>%   # build line num variable
+    ungroup() %>%
+    unnest_tokens(word, text) %>%   #Unnesting in word tokens
+    inner_join(get_sentiments("nrc"))  #  merging with sentiments in nrc
+  
+  senti_count<-senti%>% count(sentiment, index = Doc_No %/% 1, sort = FALSE) 
+  
+  # senti_df = data.frame(senti_count %>% spread(sentiment, n, fill = 0))  #reshaping by sentiments
+  
+  return(list(senti, senti_count))  # 1 . returns (words and their corresponding sentiment in senti
+  # 2. Count of sentimentsby Document
+}
+
